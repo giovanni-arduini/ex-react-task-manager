ee802776096c42e7a8ed8cfdb0c157196a82c5f6
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-20 11:47:54 +0100

    ADD React.memo to TaskRow

--- src/Components/TaskRow.jsx
+++ src/Components/TaskRow.jsx
@@ -1,11 +1,23 @@
+import React from "react";
+
 function TaskRow({ task }) {
   return (
     <div key={task.id} className="table-row">
       <p>{task.title}</p>
-      <p className="status-col">{task.status}</p>
+      <p
+        className={
+          task.status === "To do"
+            ? "red"
+            : task.status === "Doing"
+            ? "yellow"
+            : "green"
+        }
+      >
+        {task.status}
+      </p>
       <p>{task.createdAt}</p>
     </div>
   );
 }
 
-export default TaskRow;
+export default React.memo(TaskRow);
