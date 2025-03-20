f35cc7f95d83eccb16bb36acab1d178eca26d1a0
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-20 10:05:23 +0100

    ADD task list rendering in TaskList component using GlobalContext

--- src/Pages/TaskList.jsx
+++ src/Pages/TaskList.jsx
@@ -1,8 +1,24 @@
+import GlobalContext from "../Context/GlobalContext";
+import { useContext } from "react";
 function TaskList() {
+  const { tasks } = useContext(GlobalContext);
+
   return (
     <>
-      <p>Ciao mondo!</p>
-      <p>Qui ci sarà l'elenco dei task</p>
+      <thead>
+        <tr>
+          <th>Titolo</th>
+          <th>Descrizione</th>
+          <th>Stato</th>
+        </tr>
+        {tasks.map((task) => (
+          <tr key={task.id}>
+            <td>{task.title}</td>
+            <td>{task.description}</td>
+            <td>{task.status}</td>
+          </tr>
+        ))}
+      </thead>
     </>
   );
 }
