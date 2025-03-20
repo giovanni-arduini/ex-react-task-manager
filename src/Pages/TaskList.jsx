d041738b04e219fd14ca1efda44f822421c4a57e
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-20 10:10:55 +0100

    ADD GlobalContext provider and fetch tasks in App component

--- src/Pages/TaskList.jsx
+++ src/Pages/TaskList.jsx
@@ -1,8 +1,26 @@
+import GlobalContext from "../Context/GlobalContext";
+import { useContext } from "react";
 function TaskList() {
+  const { tasks } = useContext(GlobalContext);
+
   return (
     <>
-      <p>Ciao mondo!</p>
-      <p>Qui ci sarà l'elenco dei task</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Titolo</th>
+            <th>Stato</th>
+            <th>Data creazione</th>
+          </tr>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.title}</td>
+              <td>{task.status}</td>
+              <td>{task.createdAt}</td>
+            </tr>
+          ))}
+        </thead>
+      </table>
     </>
   );
 }
