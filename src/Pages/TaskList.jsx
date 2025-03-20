36f96cfce6040cf8473ec964942cb24255b6d17d
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-20 10:21:18 +0100

    ADD table in TaskList

--- src/Pages/TaskList.jsx
+++ src/Pages/TaskList.jsx
@@ -1,11 +1,11 @@
 import GlobalContext from "../Context/GlobalContext";
 import { useContext } from "react";
+
 function TaskList() {
   const { tasks } = useContext(GlobalContext);
 
   return (
     <>
-<<<<<<< HEAD
       <table>
         <thead>
           <tr>
@@ -22,22 +22,6 @@ function TaskList() {
           ))}
         </thead>
       </table>
-=======
-      <thead>
-        <tr>
-          <th>Titolo</th>
-          <th>Descrizione</th>
-          <th>Stato</th>
-        </tr>
-        {tasks.map((task) => (
-          <tr key={task.id}>
-            <td>{task.title}</td>
-            <td>{task.description}</td>
-            <td>{task.status}</td>
-          </tr>
-        ))}
-      </thead>
->>>>>>> origin/main
     </>
   );
 }
