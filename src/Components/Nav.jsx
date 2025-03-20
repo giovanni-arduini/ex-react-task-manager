0f0572fb7c92ffbc8928510f51efb3a05fc9b926
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-20 09:10:28 +0100

    ADD Nav component for navigation links

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom";
+
+function Nav() {
+  <nav>
+    <NavLink to="/">Task List</NavLink>
+    <NavLink to="/add">Add a new Task</NavLink>
+  </nav>;
+}
+
+export default Nav;
