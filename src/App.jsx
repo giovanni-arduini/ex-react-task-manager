faec3c3cb1d4120874746bdecaff23846feef473
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-20 09:54:21 +0100

    ADD GlobalContext provider and fetch tasks in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalContext from "./Context/GlobalContext";
 import TaskList from "./Pages/TaskList";
 import AddTask from "./Pages/AddTask";
 import DefaultLayout from "./Layout/DefaultLayout";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/tasks")
+      .then((response) => response.json())
+      .then((data) => setTasks(data));
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<TaskList />}></Route>
-          <Route path="/add" element={<AddTask />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <GlobalContext.Provider value={{ tasks, setTasks }}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<TaskList />}></Route>
+            <Route path="/add" element={<AddTask />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GlobalContext.Provider>
   );
 }
 
