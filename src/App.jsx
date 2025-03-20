f24175db632792a8dfcba86b3abdf69f4730958b
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-20 08:55:31 +0100

    ADD routing setup in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<TaskList />}></Route>
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
