e727fb36b3a6907fdc8f524fde3e6455bb4bd37b
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-20 10:49:24 +0100

    FIX TaskList component with TaskRow

--- src/Pages/TaskList.jsx
+++ src/Pages/TaskList.jsx
@@ -1,27 +1,27 @@
 import GlobalContext from "../Context/GlobalContext";
 import { useContext } from "react";
+import "../Components/TaskRow";
+import TaskRow from "../Components/TaskRow";
 
 function TaskList() {
   const { tasks } = useContext(GlobalContext);
 
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>Titolo</th>
-            <th>Stato</th>
-            <th>Data creazione</th>
-          </tr>
-          {tasks.map((task) => (
-            <tr key={task.id}>
-              <td>{task.title}</td>
-              <td>{task.status}</td>
-              <td>{task.createdAt}</td>
-            </tr>
-          ))}
-        </thead>
-      </table>
+      <div className="table">
+        <div>
+          <div className="table-header">
+            <p>Titolo</p>
+            <p>Stato</p>
+            <p>Data creazione</p>
+          </div>
+          <div className="table-body">
+            {tasks.map((task) => (
+              <TaskRow key={task.id} task={task} />
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
