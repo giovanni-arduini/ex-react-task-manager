39acb74aeb8ca672125dfcac5199eefda5c26714
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-20 08:57:45 +0100

    FIX App return - import TaskList

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TaskList from "./Pages/TaskList";
 
 function App() {
-  return;
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<TaskList />}></Route>
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TaskList />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
