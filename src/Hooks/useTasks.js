8b5d6220cd0e1f75550cf7fe4584c9ee9a9fa6e2
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-21 10:21:34 +0100

    ADD useTasks custom hook

---
+++ src/Hooks/useTasks.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+function useTasks() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/tasks`)
+      .then((response) => response.json())
+      .then((data) => setTasks(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return { tasks };
+}
+
+export default useTasks;
