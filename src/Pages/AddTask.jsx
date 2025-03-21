b8ae0ab9aabedb4ca4ab2ee23ba37619a6415b20
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-21 11:04:43 +0100

    ADD AddTask form & logic

--- src/Pages/AddTask.jsx
+++ src/Pages/AddTask.jsx
@@ -1,8 +1,57 @@
+import { useRef, useState } from "react";
+
 function AddTask() {
+  const [taskName, setTaskName] = useState("");
+
+  function handleNameChange(event) {
+    setTaskName(event.target.value);
+  }
+
+  const descriptionRef = useRef();
+  const statusRef = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(
+      taskName,
+      descriptionRef.current.value,
+      statusRef.current.value
+    );
+
+    setTaskName("");
+    descriptionRef.current.value = null;
+    statusRef.current.value = "toDo";
+    return;
+  }
+
   return (
     <>
-      <p>Ciao mondo!</p>
-      <p>Qui ci sarà il form di aggiunta task</p>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="task_name"
+            id="task_name"
+            placeholder="Inserisci la nuova taks"
+            value={taskName}
+            onChange={handleNameChange}
+          />
+          <input
+            type="text"
+            name="description"
+            id="description "
+            placeholder="Scrivi una descrizione"
+            ref={descriptionRef}
+          />
+          <select name="status" id="status" ref={statusRef}>
+            <option value="toDo">To Do</option>
+            <option value="doing">Doing</option>
+            <option value="done">Done</option>
+          </select>
+          <button>Aggiungi Taks</button>
+        </form>
+      </div>
     </>
   );
 }
