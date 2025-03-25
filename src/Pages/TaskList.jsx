28a694c55b77b7e0e40a45f1a62be7fe2759c9e6
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-25 15:20:07 +0100

    ADD sortHandler & sortTask function

--- src/Pages/TaskList.jsx
+++ src/Pages/TaskList.jsx
@@ -1,11 +1,41 @@
 import { GlobalContext } from "../Context/GlobalContext";
-import { useContext, useMemo } from "react";
+import { useContext, useMemo, useState } from "react";
 import "../Components/TaskRow";
 import TaskRow from "../Components/TaskRow";
 import { memo } from "react";
 
 const TaskList = memo(() => {
   const { tasks } = useContext(GlobalContext);
+  const [sortBy, setSortBy] = useState("createdAt");
+  const [sortOrder, setsortOrder] = useState(1);
+
+  const sortTask = useMemo(() => {
+    let comparison;
+
+    return [...tasks].sort((a, b) => {
+      if (sortBy === "title") {
+        comparison = a.title.localeCompare(b.title);
+      } else if (sortBy === "status") {
+        const statuses = ["To do", "Doing", "Done"];
+        comparison = statuses.indexOf(a.status) - statuses.indexOf(b.status);
+      } else if ("createdAt") {
+        const dateA = new Date(a.createdAt).getTime();
+        const dateB = new Date(b.createdAt).getTime();
+        comparison = dateA - dateB;
+      }
+
+      return comparison * sortOrder;
+    });
+  }, [tasks, sortBy, sortOrder]);
+
+  function sortHandler(check) {
+    if (sortBy === check) {
+      setsortOrder((prev) => prev * -1);
+    } else {
+      setSortBy(check);
+      setsortOrder(1);
+    }
+  }
 
   return (
     <>
@@ -13,13 +43,13 @@ const TaskList = memo(() => {
       <table>
         <thead>
           <tr>
-            <th>Titolo</th>
-            <th>Stato</th>
-            <th>Data creazione</th>
+            <th onClick={() => sortHandler("title")}>Titolo</th>
+            <th onClick={() => sortHandler("status")}>Stato</th>
+            <th onClick={() => sortHandler("createdAt")}>Data creazione</th>
           </tr>
         </thead>
         <tbody>
-          {tasks.map((task) => (
+          {sortTask.map((task) => (
             <TaskRow key={task.id} task={task} />
           ))}
         </tbody>
