1c6b94f8d8caaa74b7cd56e9a72484950662f39b
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-29 16:27:23 +0100

    ADD implement Footer component with styling and content

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,5 +1,14 @@
 function Footer() {
-  return <></>;
+  return (
+    <>
+      <footer className="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"></div>
+        <p className="color-whitetext-sm text-gray-500 sm:text-center dark:text-gray-400 sm:mt-0">
+          Qui ci andrà il pulsante per tornare su
+        </p>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
