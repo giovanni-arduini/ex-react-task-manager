d0928f1db7a91fad9dbb659b963e405dc8b37d50
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-29 16:27:14 +0100

    ADD update Nav component layout with flexbox and add dark mode toggle

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,9 +2,20 @@ import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav>
-      <NavLink to="/">Task List</NavLink>
-      <NavLink to="/add">Add a new Task</NavLink>
+    <nav className="flex justify-around p-6  m-auto bg-blue-100">
+      <div className="flex-3">
+        <NavLink className={"mx-5"} to="/">
+          Task List
+        </NavLink>
+        <NavLink className={"mx-5"} to="/add">
+          Add a new Task
+        </NavLink>
+      </div>
+      <div className="flex-1 ">
+        <div className="m-auto" src="" alt="Logo">
+          Dark mode on/off
+        </div>
+      </div>
     </nav>
   );
 }
