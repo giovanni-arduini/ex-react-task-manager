d82da38f0d6d209c900ceb06ae99d964d5428b5e
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-30 19:34:55 +0200

    ADD enhance Modal component with improved layout and styling for better usability

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -12,12 +12,24 @@ function Modal({
     <>
       {show &&
         createPortal(
-          <div className="overlay">
-            <div className="modal">
-              <h2>{title}</h2>
+          <div className="modal-overlay fixed w-full h-full z-50 opacity-80 inset-0 bg-grey-400 dark:bg-gray-700 flex align-center justify-center items-center">
+            <div className="modal-container min-w-1/3 p-8 flex flex-col bg-grey-300 dark:bg-gray-400 rounded shadow-lg">
+              <h2 className="text-center text-lg">{title}</h2>
               {content}
-              <button onClick={onConfirm}>{confirmText}</button>
-              <button onClick={onClose}>Chiudi</button>
+              <div className=" flex justify-around mt-4">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white dark:bg-sky-700 dark:hover:bg-sky-900 hover:cursor-pointer font-bold py-2 px-4 rounded"
+                  onClick={onConfirm}
+                >
+                  {confirmText}
+                </button>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white dark:bg-sky-700 dark:hover:bg-sky-900 hover:cursor-pointer font-bold py-2 px-4 rounded"
+                  onClick={onClose}
+                >
+                  Chiudi
+                </button>
+              </div>
             </div>
           </div>,
           document.body
