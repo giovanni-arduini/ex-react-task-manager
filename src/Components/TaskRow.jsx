69f82048e0dd5e820a1aba0cc3e4a2fbe51fdb10
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-30 18:44:36 +0200

    ADD enhance TaskRow component with improved styling for better visibility and interaction

--- src/Components/TaskRow.jsx
+++ src/Components/TaskRow.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function TaskRow({ task }) {
   return (
-    <tr>
-      <td className="p-2">
+    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+      <td className="p-2 text-lg hover:cursor-pointer hover:text-blue-300">
         <Link to={`/task/${task.id}`}>{task.title}</Link>
       </td>
       <td
@@ -14,7 +14,7 @@ function TaskRow({ task }) {
             : task.status === "Doing"
             ? "yellow"
             : "green"
-        } text-center p-2
+        } text-center p-2 text-black
           `}
       >
         {task.status}
