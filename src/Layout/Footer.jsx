2aba4f613a239046cac9e8f9c564c0dde4577f83
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-30 18:44:43 +0200

    ADD update Footer component styling for improved background color and dark mode support

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <>
-      <footer className="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
+      <footer className="bg-grey-400 shadow-sm dark:bg-sky-950  grow-0">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"></div>
         <p className="color-whitetext-sm text-gray-500 sm:text-center dark:text-gray-400 sm:mt-0">
           Qui ci andrà il pulsante per tornare su
