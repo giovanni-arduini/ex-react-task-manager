76aa6676502c4f94d6f62a1bee8ca3743b024f04
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-31 22:51:02 +0200

    FIX Nav & Footer flex

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,12 +3,12 @@ import ThemeToggle from "./ThemeToggle";
 
 function Nav() {
   return (
-    <nav className="flex justify-around p-6 bg-blue-200 dark:bg-sky-950 dark:text-gray-200 grow-0">
-      <div className="flex-3">
-        <NavLink className={"mx-5"} to="/">
+    <nav className="flex p-6 bg-blue-200 dark:bg-sky-950 dark:text-gray-200 grow-0">
+      <div className="flex items-center flex-3">
+        <NavLink className={" mx-5"} to="/">
           Lista Task
         </NavLink>
-        <NavLink className={"mx-5"} to="/add">
+        <NavLink className={" mx-5"} to="/add">
           Aggiungi una nuova task
         </NavLink>
       </div>
--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -3,9 +3,7 @@ function Footer() {
     <>
       <footer className="bg-grey-400 shadow-sm dark:bg-sky-950  grow-0">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"></div>
-        <p className="color-whitetext-sm text-gray-500 sm:text-center dark:text-gray-400 sm:mt-0">
-          Qui ci andrà il pulsante per tornare su
-        </p>
+        <p className="color-whitetext-sm text-gray-500 sm:text-center dark:text-gray-400 sm:mt-0"></p>
       </footer>
     </>
   );
