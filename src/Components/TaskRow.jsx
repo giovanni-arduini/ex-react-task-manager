7d8f67b53d99adfb0a5759927ea3fee99c7b4743
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-31 11:07:55 +0200

    ADD implement checkbox for task selection in TaskRow component

--- src/Components/TaskRow.jsx
+++ src/Components/TaskRow.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function TaskRow({ task }) {
+function TaskRow({ task, checked, onToggle }) {
   return (
     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+      <td className="p-2">
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={() => onToggle(task.id)}
+        />
+      </td>
       <td className="py-2 px-4 text-lg hover:cursor-pointer hover:text-blue-300">
         <Link to={`/task/${task.id}`}>{task.title}</Link>
       </td>
